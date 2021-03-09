0335299ce1f02ac7d87f4f24fae45358bdee14ab
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2021-03-09 01:17:29 +0000

    Indent with tabs

--- server/src/main/java/pt/tecnico/ttt/server/TTTServer.java
+++ server/src/main/java/pt/tecnico/ttt/server/TTTServer.java
@@ -7,35 +7,35 @@ import io.grpc.ServerBuilder;
 import java.io.IOException;
 
 public class TTTServer {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println(TTTServer.class.getSimpleName());
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.println(TTTServer.class.getSimpleName());
 
-        // receive and print arguments
-        System.out.printf("Received %d arguments%n", args.length);
-        for (int i = 0; i < args.length; i++) {
-            System.out.printf("arg[%d] = %s%n", i, args[i]);
-        }
+		// receive and print arguments
+		System.out.printf("Received %d arguments%n", args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
 
-        // check arguments
-        if (args.length < 1) {
-            System.err.println("Argument(s) missing!");
-            System.err.printf("Usage: java %s port%n", TTTServer.class.getName());
-            return;
-        }
+		// check arguments
+		if (args.length < 1) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s port%n", TTTServer.class.getName());
+			return;
+		}
 
-        final int port = Integer.parseInt(args[0]);
-        final BindableService impl = new TTTServiceImpl();
+		final int port = Integer.parseInt(args[0]);
+		final BindableService impl = new TTTServiceImpl();
 
-        // Create a new server to listen on port
-        Server server = ServerBuilder.forPort(port).addService(impl).build();
+		// Create a new server to listen on port
+		Server server = ServerBuilder.forPort(port).addService(impl).build();
 
-        // Start the server
-        server.start();
+		// Start the server
+		server.start();
 
-        // Server threads are running in the background.
-        System.out.println("Server started");
+		// Server threads are running in the background.
+		System.out.println("Server started");
 
-        // Do not exit the main thread. Wait until server is terminated.
-        server.awaitTermination();
-    }
+		// Do not exit the main thread. Wait until server is terminated.
+		server.awaitTermination();
+	}
 }
--- server/src/main/java/pt/tecnico/ttt/server/TTTServiceImpl.java
+++ server/src/main/java/pt/tecnico/ttt/server/TTTServiceImpl.java
@@ -4,36 +4,36 @@ import io.grpc.stub.StreamObserver;
 import pt.tecnico.ttt.*;
 
 public class TTTServiceImpl extends TTTGrpc.TTTImplBase {
-    private TTTGame ttt = new TTTGame();
+	private TTTGame ttt = new TTTGame();
 
-    @Override
-    public void currentBoard(CurrentBoardRequest request, StreamObserver<CurrentBoardResponse> responseObserver) {
-        // StreamObserver is used to represent the gRPC stream between the server and client
-        // in order to send the appropriate responses (or errors, if any occur)
-        CurrentBoardResponse response = CurrentBoardResponse.newBuilder().setBoard(ttt.toString()).build();
-        // Send a single response through the stream
-        responseObserver.onNext(response);
-        // Notify the client that the operation has been completed
-        responseObserver.onCompleted();
-    }
+	@Override
+	public void currentBoard(CurrentBoardRequest request, StreamObserver<CurrentBoardResponse> responseObserver) {
+		// StreamObserver is used to represent the gRPC stream between the server and client
+		// in order to send the appropriate responses (or errors, if any occur)
+		CurrentBoardResponse response = CurrentBoardResponse.newBuilder().setBoard(ttt.toString()).build();
+		// Send a single response through the stream
+		responseObserver.onNext(response);
+		// Notify the client that the operation has been completed
+		responseObserver.onCompleted();
+	}
 
-    @Override
-    public void checkWinner(CheckWinnerRequest request, StreamObserver<CheckWinnerResponse> responseObserver) {
-        CheckWinnerResponse response = CheckWinnerResponse.newBuilder().setResult(ttt.checkWinner()).build();
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-    }
+	@Override
+	public void checkWinner(CheckWinnerRequest request, StreamObserver<CheckWinnerResponse> responseObserver) {
+		CheckWinnerResponse response = CheckWinnerResponse.newBuilder().setResult(ttt.checkWinner()).build();
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
 
-    @Override
-    public void play(PlayRequest request, StreamObserver<PlayResponse> responseObserver) {
-        PlayResponse response = PlayResponse.newBuilder()
-                .setResult(
-                    ttt.play(
-                        request.getRow(),
-                        request.getColumn(),
-                        request.getPlayer()))
-                .build();
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-    }
+	@Override
+	public void play(PlayRequest request, StreamObserver<PlayResponse> responseObserver) {
+		PlayResponse response = PlayResponse.newBuilder()
+				.setResult(
+					ttt.play(
+						request.getRow(),
+						request.getColumn(),
+						request.getPlayer()))
+				.build();
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
 }
